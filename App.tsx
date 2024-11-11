58ebd8d3a1d698e26037ad9aaf492c2d2ae62815
shahidkhan0085200@gmail.com <130751667+shahid-khan-codegenio@users.noreply.github.com>
2024-11-11 09:14:48 +0500

    update

---
+++ App.tsx
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function App(): React.JSX.Element {
+  return (
+    <View>
+      <Text>Hello Word</Text>
+      <Text>Shahid khan</Text>
+    </View>
+  );
+}
+
+export default App;
